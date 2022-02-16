a1cbbc38124c766be3322d1df1ce196d0db53f95
Julian <81520713+Juliandev02@users.noreply.github.com>
2022-02-16 15:41:58 +0100

    Changed private dir

--- example/install.py
+++ example/install.py
@@ -1,4 +1,4 @@
 from sup3.installer import create_directory, do_install
 
-create_directory("D:\Test")
-do_install("https://github.com/Strawberry-Software-Industries/SecureCloud/releases/download/v1.3/SecureCloud-Server_1.3-LTS_Win64-bin.7z", "D:\Test", "SecureCloud-Server_1.3-LTS_Win64-bin.7z")
+create_directory("C:\Test")
+do_install("https://github.com/Strawberry-Software-Industries/SecureCloud/releases/download/v1.3/SecureCloud-Server_1.3-LTS_Win64-bin.7z", "C:\Test", "SecureCloud-Server_1.3-LTS_Win64-bin.7z")
