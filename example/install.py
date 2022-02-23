48224f43edcbd7aa75ec1e4c467f59c01dca6cd3
Julian <81520713+Juliandev02@users.noreply.github.com>
2022-02-23 22:27:20 +0100

    Add files via upload

--- example/install.py
+++ example/install.py
@@ -1,4 +1,7 @@
-from sup3.installer import create_directory, do_install
+from sup3.installer import create_directory, do_install, extract, wait_after_installation, finish_installation
 
-create_directory("C:\Test")
-do_install("https://github.com/Strawberry-Software-Industries/SecureCloud/releases/download/v1.3/SecureCloud-Server_1.3-LTS_Win64-bin.7z", "C:\Test", "SecureCloud-Server_1.3-LTS_Win64-bin.7z")
+create_directory("D:\Test")
+do_install("https://github.com/Strawberry-Software-Industries/SecureCloud/releases/download/v1.3/SecureCloud-Server_1.3-LTS_Win64-bin.7z", "D:\Test", "SecureCloud-Server_1.3-LTS_Win64-bin.7z")
+extract("D:\Test\SecureCloud-Server_1.3-LTS_Win64-bin.7z", "D:\Test")
+finish_installation()
+wait_after_installation()
--- installer.py
+++ installer.py
@@ -6,6 +6,8 @@ from colorama import Fore
 import time
 import datetime
 from halo import Halo
+import zipfile
+import py7zr
 
 class color:
    PURPLE = '\033[95m'
@@ -24,13 +26,34 @@ def create_directory(dirpath=""):
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
 
+
 def do_install(url="", install_dir="", filename=""):
     spinner = Halo(text=f'{color.YELLOW + color.BOLD}Downloading {url}{color.END}', spinner='bouncing')
     spinner.start()
     r = requests.get(url, allow_redirects=True)
     print(f"{color.GREEN}\nFinished. {color.END}")
-    spinner = Halo(text=f'{color.YELLOW + color.BOLD}Installing {color.END}', spinner='bouncing')
-    spinner.start()
+    spinner.stop()
     os.chdir(install_dir)
     open(filename, 'wb').write(r.content)
-    print(f"{color.GREEN}\nInstalled! {color.END}")
+
+
+    
+def extract(filename="", extract_dir=""):
+    fname, file_extension = os.path.splitext(filename)
+    spinner = Halo(text=f'{color.YELLOW + color.BOLD}Installing {color.END}', spinner='bouncing')
+    print(f'{color.YELLOW + color.BOLD}Extracting to {extract_dir} {color.END}')
+    spinner.start()
+    if file_extension == ".zip":
+        with zipfile.ZipFile(filename, 'r') as zip:
+            zip.extractall(extract_dir)
+    elif file_extension == ".7z":
+        with py7zr.SevenZipFile(filename, mode='r') as sevenz:
+            sevenz.extractall(path=extract_dir)
+    spinner.stop()
+
+
+def wait_after_installation():
+    time.sleep(30)
+
+def finish_installation():
+    print(f"{color.GREEN}Installed! Closing in 30 Seconds{color.END}")
